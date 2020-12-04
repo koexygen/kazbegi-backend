ade1f8dbda2c9ad46e9d7a5fdaf1a46177067b34
Koexygen <rexarkoexygen@gmail.com>
2020-12-05 02:08:13 +0400

    practice update

--- README.md
+++
@@ -1,1 +0,0 @@
-# kazbegi-backend
--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-    res.status(200).json({msg:'Hello from the server sides'});
-})
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-})
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 👋');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// 3) ROUTES
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
