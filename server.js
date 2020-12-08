36ca0e3407753a67e4c0d7f498eece4a45e71514
Koexygen <rexarkoexygen@gmail.com>
2020-12-09 00:45:52 +0400

    keeping work on kazbegi back

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
-const dotenv = require('dotenv');
-const app = require('./app');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = require("./app");
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: "./config.env" });
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(con => {
+    console.log(con);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
