91923e85a2616c0a9f23a9099236f08a82d35a46
Koexygen <rexarkoexygen@gmail.com>
2020-12-09 06:35:06 +0400

    refactor models

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,20 +1,58 @@
 const mongoose = require("mongoose");
 
 const tourSchema = new mongoose.Schema({
-  rating: {
+  ratingsAverage: {
     type: Number,
     default: 4.5
+  },
+  ratingsCount: {
+    type: Number,
+    default: 0
+  },
+  duration: {
+    type: Number,
+    required: [true, "duration is required"]
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "groupSize is required"]
+  },
+  difficulty: {
+    type: String,
+    required: [true, "difficulty is required"],
+    trim: true
   },
   name: {
     type: String,
     required: [true, "tour must have a Name"],
-    unique: true
+    unique: true,
+    trim: true
   },
   price: {
     type: Number,
     required: [true, "tour must have a price"],
     min: 150
-  }
+  },
+  priceDiscount: Number,
+  summery: {
+    type: String,
+    trim: true,
+    required: [true, "tour must have description"]
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: [true, "tour must have cover image"]
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDate: [Date]
 });
 
 const Tour = mongoose.model("Tour", tourSchema);
