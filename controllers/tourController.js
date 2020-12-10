51aa514ebe162ac76ac40892c705dbdc17953774
Koexygen <rexarkoexygen@gmail.com>
2020-12-10 04:47:36 +0400

    get all tours api limits

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -19,6 +19,15 @@ exports.getAllTours = async (req, res) => {
     } else {
       query.sort("-createdAt");
     }
+
+    //ლიმიტი
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
     const tours = await query;
 
     res.status(200).json({
