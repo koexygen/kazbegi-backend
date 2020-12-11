c62e8199cf422562219e4bdc2727eb6953fac074
Koexygen <rexarkoexygen@gmail.com>
2020-12-11 05:22:50 +0400

    getMonthlyPlan aggregations

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const tourController = require("./../controllers/tourController");
 
 const router = express.Router();
+router.route("/monthly-plan/:year").get(tourController.getMonthlyPlan);
 router.route("/tour-stats").get(tourController.getTourStats);
 
 router
