cf0531848d60f95ae479e680429e7a7cf74334ab
Koexygen <rexarkoexygen@gmail.com>
2020-12-13 21:12:11 +0400

    basic error handling

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
@@ -15,11 +17,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log("Hello from the middleware 👋");
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -29,21 +26,11 @@ app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
 app.use("*", (req, res, next) => {
-  const err = new Error(`url requested ${req.originalUrl} has not been found`);
-  err.status = "fail";
-  err.statusCode = 404;
-
-  next(err);
+  next(
+    new AppError(`url requested ${req.originalUrl} has not been found`, 404)
+  );
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "fail";
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
