8fcdffc427d82bd49486c01c53ff1f984b1f22fc
Koexygen <rexarkoexygen@gmail.com>
2020-12-14 04:37:50 +0400

    error dev/prod

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,35 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+  } else {
+    console.error(err);
+
+    res.status(500).json({
+      status: "fail",
+      message: "something went wrong...!"
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "fail";
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+  if (process.env.NODE_EN === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    sendErrorProd(err, res);
+  }
 };
