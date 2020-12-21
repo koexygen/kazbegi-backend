51b441e7c31ef592bd25ffd315ac7e6836c18feb
Koexygen <rexarkoexygen@gmail.com>
2020-12-21 07:24:18 +0400

    User signup and password hashing done.

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -24,8 +25,22 @@ const userSchema = new mongoose.Schema({
   },
   passwordConfirm: {
     type: String,
-    required: [true, "Please confirm a password"]
+    required: [true, "Please confirm a password"],
+    validate: {
+      validator: function(value) {
+        return value === this.password;
+      },
+      message: "Password confirmation failed"
+    }
   }
+});
+
+userSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
