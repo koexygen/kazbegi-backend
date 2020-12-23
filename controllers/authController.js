8e2ee747e7154661c987cfd92d97bc0858e3d57f
Koexygen <rexarkoexygen@gmail.com>
2020-12-23 09:39:02 +0400

    user login and pass validation done

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,12 @@
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
 const jwt = require("jsonwebtoken");
+const AppError = require("./../utils/appError");
+
+const signToken = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
 exports.signUp = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -10,9 +16,7 @@ exports.signUp = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
   });
 
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  const token = signToken(newUser._id);
 
   res.status(201).json({
     status: "success",
@@ -20,5 +24,26 @@ exports.signUp = catchAsync(async (req, res, next) => {
     data: {
       user: newUser,
     },
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new AppError("please provide a valid email or password"), 400);
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user || !(await User.correctPassword(password, user.password))) {
+    return next(new AppError("Invalid email or password"), 401);
+  }
+
+  const token = signToken(user._id);
+
+  res.status(201).json({
+    status: "success",
+    token,
   });
 });
--- models/userModel.js
+++ models/userModel.js
@@ -22,6 +22,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please provide a password"],
     minlength: 6,
+    select: false,
   },
   passwordConfirm: {
     type: String,
@@ -42,6 +43,13 @@ userSchema.pre("save", async function (next) {
   this.passwordConfirm = undefined;
   next();
 });
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model("User", userSchema);
 
