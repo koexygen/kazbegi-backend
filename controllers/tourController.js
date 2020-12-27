451691b7e9ed4970dfa4035654085e647741bce5
Koexygen <rexarkoexygen@gmail.com>
2020-12-27 21:44:36 +0400

    implemented nested routes for reviews

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -39,6 +39,9 @@ exports.getTour = catchAsync(async (req, res, next) => {
 });
 
 exports.createTour = catchAsync(async (req, res, next) => {
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+
   const tour = await Tour.create(req.body);
 
   res.status(201).json({
