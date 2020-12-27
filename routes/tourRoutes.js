d399d03a8371d0e819295633a64a6805e55d343e
Koexygen <rexarkoexygen@gmail.com>
2020-12-28 00:07:25 +0400

    reworking nested routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const tourController = require("./../controllers/tourController");
 const authController = require("./../controllers/authController");
-const reviewController = require("./../controllers/reviewController");
+const reviewRouter = require("./../routes/reviewRoutes");
 
 const router = express.Router();
+
+router.use("/:tourId/reviews", reviewRouter);
+
 router.route("/monthly-plan/:year").get(tourController.getMonthlyPlan);
 router.route("/tour-stats").get(tourController.getTourStats);
 
@@ -24,14 +27,6 @@ router
     authController.protect,
     authController.restrictTo("admin"),
     tourController.deleteTour
-  );
-
-router
-  .route("/:tourId/reviews")
-  .post(
-    authController.protect,
-    authController.restrictTo("user"),
-    reviewController.createReview
   );
 
 module.exports = router;
