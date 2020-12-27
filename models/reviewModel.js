faab1080d39f3cf57f2314c39647d486d248372f
Koexygen <rexarkoexygen@gmail.com>
2020-12-27 08:20:49 +0400

    created reviewModel.js

---
+++ models/reviewModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: { type: String, required: [true, "Review Cannot be empty"] },
+    rating: {
+      type: Number,
+      min: [1, "Minimum rating allowed 1"],
+      max: [5, "Maximum rating allowed 5"],
+    },
+    createdAt: { type: Date, default: Date.now },
+    author: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Review must have an author."],
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Tour",
+      required: [true, "Review must belong to a Tour"],
+    },
+  },
+
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const Review = mongoose.model("Review", reviewSchema);
+module.exports = Review;
