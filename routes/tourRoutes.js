6fdb7ec8326f48b779ba86ac9c4005b15b8217db
Koexygen <rexarkoexygen@gmail.com>
2020-12-28 04:12:48 +0400

    improved authentication and permissions

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,7 +7,13 @@ const router = express.Router();
 
 router.use("/:tourId/reviews", reviewRouter);
 
-router.route("/monthly-plan/:year").get(tourController.getMonthlyPlan);
+router
+  .route("/monthly-plan/:year")
+  .get(
+    authController.protect,
+    authController.restrictTo("admin", "lead-guide", "guide"),
+    tourController.getMonthlyPlan
+  );
 router.route("/tour-stats").get(tourController.getTourStats);
 
 router
@@ -16,13 +22,21 @@ router
 
 router
   .route("/")
-  .get(authController.protect, tourController.getAllTours)
-  .post(tourController.createTour);
+  .get(tourController.getAllTours)
+  .post(
+    authController.protect,
+    authController.restrictTo("admin", "lead-guide"),
+    tourController.createTour
+  );
 
 router
   .route("/:id")
   .get(tourController.getTour)
-  .patch(tourController.updateTour)
+  .patch(
+    authController.protect,
+    authController.restrictTo("admin", "lead-guide"),
+    tourController.updateTour
+  )
   .delete(
     authController.protect,
     authController.restrictTo("admin"),
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,25 +6,22 @@ const router = express.Router();
 
 router.post("/signup", authController.signUp);
 router.post("/login", authController.login);
-router.patch(
-  "/updateMyPassword",
-  authController.protect,
-  authController.updatePassword
-);
-router.get(
-  "/me",
-  authController.protect,
-  userController.getMe,
-  userController.getUser
-);
-router.patch("/updateMe", authController.protect, userController.updateMe);
-router.delete("/deleteMe", authController.protect, userController.deleteMe);
-
 router.post("/forgotPassword", authController.forgotPassword);
 router.patch("/resetPassword/:token", authController.resetPassword);
 
-router.route("/").get(userController.getAllUsers);
+router.use(authController.protect);
 
+router.patch("/updateMyPassword", authController.updatePassword);
+
+router.get("/me", userController.getMe, userController.getUser);
+
+router.patch("/updateMe", userController.updateMe);
+router.delete("/deleteMe", userController.deleteMe);
+
+//restricted to admins
+router.use(authController.restrictTo("admin"));
+
+router.route("/").get(userController.getAllUsers);
 router
   .route("/:id")
   .get(userController.getUser)
